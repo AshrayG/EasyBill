6b9e69aa67369e1c0fa0b9ecacdc1d60ee1ad696
AshG-1 <ashray.gupta2002@gmail.com>
2022-08-22 09:08:57 +0530

    first commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,8 +17,7 @@ function About() {
                 </div>
                 <div className="textabout">
                     We are still in developing Phase and there's always a better version of the product is on it's way.
-                    We are a group of 3 highly enthusiastic developers built "EasyMint" - A Seamless NFT minting platform on Tezos. Using React as front-end, the web app is highly responsive and smooth to mint.
-                    Features such as Multiple NFTs minting and NFT minting to other address are implemented and fully operational, Many more exciting features will be added soon.
+                    To remove all the discrepancies in the warranty and billing space, we at easybilling focuses on Minting warranty cards on blockchain in the NFT space.
                 </div>
                 <div className="social-icons"><a href="https://twitter.com/EasyMint001" target="_blank" className="social-link twitter-link "><img src={twitter} width={36} height={36} alt="" /></a>
                     <a href="https://www.facebook.com/Easy-mint-102723372554502" target="_blank" className="social-link facebook-link "><img src={fb} width={36} height={36} alt="" /></a>
